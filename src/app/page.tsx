0ba7dcc5633eb57ada29ee9694f8a752225bd197
GregoireDuhem <gregoire.duhem@epitech.eu>
2025-06-08 12:10:54 +0200

    [.] fix weird bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "./Hero";
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden w-dvh min-h-screen">
+    <div className="overflow-x-hidden w-screen min-h-screen">
       <Hero />
     </div>
   );
