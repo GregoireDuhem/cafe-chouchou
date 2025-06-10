fd93ef6b767251d41e6fba34bb21af9d345832e1
Greg <gregoire.duhem@epitech.eu>
2025-06-10 14:08:29 +0200

    feat: add Lenis smooth scrolling provider and update dependencies

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -2,12 +2,13 @@
 
 import TransitionOverlay from "./components/TransisionOverlay";
 import { LoaderProvider } from "./components/LoaderContext";
+import LenisProvider from "./components/LenisProvider";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <LoaderProvider>
       <TransitionOverlay />
-      {children}
+      <LenisProvider>{children}</LenisProvider>
     </LoaderProvider>
   );
 }
